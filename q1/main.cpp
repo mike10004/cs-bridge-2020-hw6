584336b2eb7a0536cd253d2d20de760ff0bc0851
Mike Chaberski <mchaberski@gmail.com>
2020-02-17 21:12:57 -0500

    q1 complete

--- q1/main.cpp
+++ q1/main.cpp
@@ -1,11 +1,32 @@
-// <author>
-// Question 1
+// mac937@nyu.edu
+// HW6 Question 1
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
+int fib(int n) {
+    assert(n > 0);
+    assert(n <= 46);  // die on int overflow
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    int fNMinus1 = 1, fNMinus2 = 1;
+    int fN = 0;
+    for (int i = 3; i <= n; i++) {
+        fN = fNMinus1 + fNMinus2;
+        fNMinus2 = fNMinus1;
+        fNMinus1 = fN;
+    }
+    return fN;
+}
+
 int main() {
-    cout << "q1 executed" << endl;
+    int userInput;
+    cout << "Please enter a positive integer: ";
+    cin >> userInput;
+    
+    cout << fib(userInput) << endl;    
     return 0;
 }
