d9ae5ea45ea93e2b5707e73e89ee9dea83f6000e
Mike Chaberski <mchaberski@gmail.com>
2020-02-18 22:30:44 -0500

    q3: implement

--- q3/main.cpp
+++ q3/main.cpp
@@ -5,7 +5,21 @@
 
 using namespace std;
 
+double eApprox(int n) {
+    double nFactorial = 1;
+    double sum = 0;
+    for (int i = 0; i <= n; i++) {
+        double addend = 1 / nFactorial;
+        sum += addend;
+        nFactorial *= (i + 1);
+    }
+    return sum;
+}
+
 int main() {
-    cout << "q3 executed" << endl;
+    cout.precision(30);
+    for (int n = 1; n <= 15; n++) {
+        cout<<"n = "<<n<<'\t'<<eApprox(n)<<endl;
+    }
     return 0;
 }
